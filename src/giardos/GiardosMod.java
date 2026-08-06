5f9430ae412851ff062358340e37b06f102c0438
Rain <capueinedaffe@gmail.com>
2026-08-05 22:42:07 -0400

    Create GiardosMod class with loadContent method
    Initial implementation of GiardosMod with loadContent method.

---
+++ src/giardos/GiardosMod.java
@@ -0,0 +1,11 @@
+package giardos;
+
+import mindustry.mod.*;
+
+public class GiardosMod extends Mod{
+    @Override
+    public void loadContent(){
+        // Por ahora el contenido se carga automáticamente desde assets/content/
+        // Aquí registraremos planetas y lógica custom en futuras versiones
+    }
+}
