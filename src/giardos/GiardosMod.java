b33dbf744e079d56be7f70146a0657c79c9addcd
Rain <capueinedaffe@gmail.com>
2026-08-06 20:33:29 -0400

    Refactor GiardosMod loadContent method

--- src/giardos/GiardosMod.java
+++ src/giardos/GiardosMod.java
@@ -6,26 +6,23 @@ import mindustry.content.TechTree;
 import mindustry.type.UnitType;
 import mindustry.Vars;
 
-import static mindustry.content.TechTree.*;
-
-public class GiardosMod extends Mod {
+public class GiardosMod extends mindustry.mod.Mod {
 
     @Override
     public void loadContent() {
         Log.info("[Giardos] Cargando contenido...");
         
-        // 1. Cargar items PRIMERO
         GiardosItems.load();
         Log.info("[Giardos] Biomasa registrada: " + GiardosItems.biomass.name);
         
-        // 2. Añadir biomasa al tech tree
-        nodeProduce(GiardosItems.biomass, Seq::new);
+        TechTree.nodeProduce(GiardosItems.biomass, Seq::new);
         Log.info("[Giardos] Biomasa añadida al tech tree");
         
-        // 3. Modificar mineItems de unidades
         UnitType vyper = Vars.content.unit("giardos-vyper");
         if (vyper != null) {
-            if (vyper.mineItems == null) vyper.mineItems = new Seq<>();
+            if (vyper.mineItems == null) {
+                vyper.mineItems = new Seq<>();
+            }
             if (!vyper.mineItems.contains(GiardosItems.biomass)) {
                 vyper.mineItems.add(GiardosItems.biomass);
                 Log.info("[Giardos] Biomasa añadida a Vyper");
@@ -34,7 +31,9 @@ public class GiardosMod extends Mod {
         
         UnitType harvester = Vars.content.unit("giardos-mutatio-harvester");
         if (harvester != null) {
-            if (harvester.mineItems == null) harvester.mineItems = new Seq<>();
+            if (harvester.mineItems == null) {
+                harvester.mineItems = new Seq<>();
+            }
             if (!harvester.mineItems.contains(GiardosItems.biomass)) {
                 harvester.mineItems.add(GiardosItems.biomass);
                 Log.info("[Giardos] Biomasa añadida a Mutatio Harvester");
@@ -42,45 +41,5 @@ public class GiardosMod extends Mod {
         }
         
         Log.info("[Giardos] Mod cargado correctamente.");
-    }
-}
-            
-            if (coreShard != null) {
-                // Crear nodo para biomasa como hijo de core-shard
-                TechNode biomassNode = new TechNode(coreShard, biomass, ItemStack.with(Items.copper, 100));
-                biomassNode.name = "Biomasa";
-                Log.info("[Giardos] Nodo de tech tree creado para biomasa");
-            } else {
-                Log.warn("[Giardos] No se encontró core-shard para tech tree");
-            }
-            
-            // === 3. AÑADIR BIOMASA A MINEITEMS DE UNIDADES ===
-            // Vyper
-            UnitType vyper = Vars.content.unit("giardos-vyper");
-            if (vyper != null) {
-                if (vyper.mineItems == null) {
-                    vyper.mineItems = new Seq<>();
-                }
-                if (!vyper.mineItems.contains(biomass)) {
-                    vyper.mineItems.add(biomass);
-                    Log.info("[Giardos] Biomasa añadida a mineItems de Vyper");
-                }
-            }
-            
-            // Mutatio Harvester
-            UnitType harvester = Vars.content.unit("giardos-mutatio-harvester");
-            if (harvester != null) {
-                if (harvester.mineItems == null) {
-                    harvester.mineItems = new Seq<>();
-                }
-                if (!harvester.mineItems.contains(biomass)) {
-                    harvester.mineItems.add(biomass);
-                    Log.info("[Giardos] Biomasa añadida a mineItems de Mutatio Harvester");
-                }
-            }
-            
-        } else {
-            Log.err("[Giardos] ERROR: Item biomasa no encontrado!");
-        }
     }
 }
